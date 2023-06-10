543053c03bad3f8daa971d56b48de6c81d01aae1
victor zamora <victorzamorac98@gmail.com>
2023-06-10 13:12:46 -0400

    Security Config

---
+++ src/main/java/com/victordev/encuesta/models/requests/UserLoginRequestModel.java
@@ -0,0 +1,17 @@
+package com.victordev.encuesta.models.requests;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class UserLoginResquestModel {
+    @NotEmpty
+    @Email
+    private String email;
+    @NotEmpty
+    @Size(min = 8, max = 40)
+    private String password;
+}
